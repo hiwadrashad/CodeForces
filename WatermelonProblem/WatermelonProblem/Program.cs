eb916196786164026f8037db003a9c7904670afc
hiwadrashad <41074761+hiwadrashad@users.noreply.github.com>
2021-03-31 17:40:53 +0200

    Add files via upload
    Initial

---
+++ WatermelonProblem/WatermelonProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WatermelonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int number = Int32.Parse(input);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+
+            }
+        }
+    }
+}
